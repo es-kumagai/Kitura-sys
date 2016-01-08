300c89da05fe809b11deccc9beb97b819c539885
Daniel Firsht <dfirsht@us.ibm.com>
2016-01-08 12:17:06 -0600

    resolve merge conflict

--- StringUtils.swift
+++ StringUtils.swift
@@ -5,24 +5,31 @@
 //  Created by Samuel Kallner on 11/15/15.
 //
 //
+#if os(OSX) || os(iOS)
+    import Darwin
+#elseif os(Linux)
+    import Glibc
+#endif
 
 import Foundation
 
 public class StringUtils {
     
     public static func toUtf8String(str: String) -> NSData? {
-        let data = str.dataUsingEncoding(NSUTF8StringEncoding)
+        let nsstr:NSString = str.bridge()
+        let data = nsstr.dataUsingEncoding(NSUTF8StringEncoding)
         return data
     }
     
     public static func toNullTerminatedUtf8String(str: String) -> NSData? {
-        let cString = str.cStringUsingEncoding(NSUTF8StringEncoding)
-        let data = NSData(bytes: cString!, length: Int(strlen(cString!))+1)
+        let nsstr:NSString = str.bridge()
+        let cString = nsstr.cStringUsingEncoding(NSUTF8StringEncoding)
+        let data = NSData(bytes: cString, length: Int(strlen(cString))+1)
         return data
     }
     
     public static func fromUtf8String(data: NSData) -> String? {
         let str = NSString(data: data, encoding: NSUTF8StringEncoding)
-        return str as? String
+        return str!.bridge()
     }
 }
