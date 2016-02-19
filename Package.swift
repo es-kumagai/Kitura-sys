e15b5644dd6635d96818d4b90682ca7b896aac87
rolivieri <roliv@us.ibm.com>
2016-02-19 15:22:20 -0600

    Updated dependency version numbers.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Sys",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
+            .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
             //.Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
         ]
 )
