5661e7c70359bc99ff096ae8ef262e06783024ad
rolivieri <roliv@us.ibm.com>
2016-02-19 13:30:18 -0600

    Updates git URLs.

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ import PackageDescription
 let package = Package(
     name: "Kitura-Sys",
         dependencies: [
-            //.Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
-            .Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
+            .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
+            //.Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
         ]
 )
