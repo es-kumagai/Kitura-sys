b421d66657c46fc3991506ab5559eee96b4ede4b
Shmuel Kallner <kallner@il.ibm.com>
2016-02-19 10:26:25 -0600

    Renamed module sys to KituraSys

--- Package.swift
+++ Package.swift
@@ -17,10 +17,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "sys",
+    name: "Kitura-Sys",
         dependencies: [
             //.Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", majorVersion: 0),
             .Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
         ]
 )
-
