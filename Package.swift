997723d54d4e8f4f209a08196f109be3ac3fe522
rolivieri <roliv@us.ibm.com>
2016-02-24 11:39:14 -0600

    IBM-Swift/Kitura#156 Updating master repo for sys.

--- Package.swift
+++ Package.swift
@@ -20,6 +20,5 @@ let package = Package(
     name: "Kitura-Sys",
         dependencies: [
             .Package(url: "https://github.com/IBM-Swift/LoggerAPI.git", versions: Version(0,2,0)..<Version(0,3,0)),
-            //.Package(url: "git@github.com:IBM-Swift/LoggerAPI.git", majorVersion: 0),
         ]
 )
